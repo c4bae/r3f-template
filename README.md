174b09b0242dda59fff2f566e7e5e1e3f245cc12
c4bae <123281198+c4bae@users.noreply.github.com>
2025-10-17 19:27:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Three Fiber template
+
+This template includes R3F, Drei, and Leva. Rapier may be added later.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      React Three Fiber template by c4bae, including Leva and Drei.
+    </div>
+  )
+}
+
+export default App
